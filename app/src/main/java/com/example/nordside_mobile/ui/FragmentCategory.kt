bd8c45d6d10d15dfec5b2c4fa0eac513beacad65
Oleg <unxxxt@gmail.com>
2021-10-31 04:27:00 +0300

    add progressBar and internet connection checking into FragmentPartner and FragmentCategory

--- app/src/main/java/com/example/nordside_mobile/ui/FragmentCategory.kt
+++ app/src/main/java/com/example/nordside_mobile/ui/FragmentCategory.kt
@@ -2,10 +2,12 @@ package com.example.nordside_mobile.ui
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
 import androidx.cardview.widget.CardView
@@ -16,6 +18,7 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.lifecycle.Observer
 import com.example.nordside_mobile.R
 import com.example.nordside_mobile.model.Category
+import com.example.nordside_mobile.model.ServerToken
 import com.example.nordside_mobile.repository.Resource
 import com.example.nordside_mobile.viewmodel.FragmentCategoryViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -46,11 +49,28 @@ class FragmentCategory : Fragment(R.layout.fragment_category) {
         recyclerView.layoutManager =
             LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
 
+
+
+        val progressBar = view.findViewById<ProgressBar>(R.id.progress_circular_category)
+        progressBar.visibility = View.VISIBLE
         val categoryListLiveData = categoryViewModel.categoryListLiveData
 
+
         categoryListLiveData.observe(viewLifecycleOwner, Observer {
-            recyclerView.adapter = CategoryAdapter(it.data!!)
+            progressBar.visibility = View.INVISIBLE
+            when (categoryListLiveData.value) {
+                is Resource.Success -> {
+                    recyclerView.adapter = CategoryAdapter(it.data!!)
+                }
+                is Resource.Error -> {
+                    val errorTextView = view.findViewById<TextView>(R.id.error_message_category)
+                    errorTextView.text =
+                        (categoryListLiveData.value as Resource.Error<List<Category>>).message
+                    errorTextView.visibility = View.VISIBLE
+                }
+            }
         })
+
 
 
     }
@@ -108,7 +128,15 @@ class FragmentCategory : Fragment(R.layout.fragment_category) {
     }
 
 
-    private fun showErrorMessage(message: String) {
+    private fun showErrorMessage(message: String?) {
+
+    }
+
+    private fun hideProgressBar() {
+
+    }
+
+    private fun showProgressBar() {
 
     }
 
